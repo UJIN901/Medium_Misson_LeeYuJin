e039f15fdcbe8e5248cca71c34eac3d397df59c6
UJIN901 <dalvit9655@gmail.com>
2023-12-12 13:18:36 +0900

    feat: JoinForm class 추가

--- src/main/java/com/ll/medium/domain/member/member/controller/MemberController.java
+++ src/main/java/com/ll/medium/domain/member/member/controller/MemberController.java
@@ -1,8 +1,13 @@
 package com.ll.medium.domain.member.member.controller;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -13,5 +18,22 @@ public class MemberController {
     @GetMapping("/join")
     public String join() {
         return "domain/member/member/join";
+    }
+
+    @Getter
+    @Setter
+    public static class JoinForm{
+        @NotBlank
+        private String username;
+        @NotBlank
+        private String password;
+        @NotBlank
+        private String passwordConfirm;
+    }
+    @PostMapping("/join")
+    public String signup(@Valid JoinForm joinForm) {
+        
+
+        return "redirect:/";
     }
 }
