563a56dd766af0c606a51f0d778b928334cf5702
UJIN901 <dalvit9655@gmail.com>
2023-12-12 14:02:55 +0900

    feat: 기본적인 member 도메인 추가
    - entity, service, repository

--- src/main/java/com/ll/medium/domain/member/member/controller/MemberController.java
+++ src/main/java/com/ll/medium/domain/member/member/controller/MemberController.java
@@ -1,5 +1,7 @@
 package com.ll.medium.domain.member.member.controller;
 
+import com.ll.medium.domain.member.member.entity.Member;
+import com.ll.medium.domain.member.member.service.MemberService;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/member")
 @RequiredArgsConstructor
 public class MemberController {
+    private final MemberService memberService;
 
     @GetMapping("/join")
     public String join() {
@@ -32,8 +35,10 @@ public class MemberController {
     }
     @PostMapping("/join")
     public String signup(@Valid JoinForm joinForm) {
-        
+        Member member = memberService.join(joinForm.getUsername(), joinForm.getPassword());
 
-        return "redirect:/";
+        long id = member.getId();
+
+        return "redirect:/?msg=No %d member joined".formatted(id);
     }
 }
