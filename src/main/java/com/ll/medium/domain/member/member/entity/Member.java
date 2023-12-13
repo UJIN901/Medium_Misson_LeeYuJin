87d442f6e2036319e8567a65b06d6cd7c6ecc59a
UJIN901 <dalvit9655@gmail.com>
2023-12-13 10:22:23 +0900

    feat: admin 페이지 추가
    - move: getAuthorities 기능 service -> member"

--- src/main/java/com/ll/medium/domain/member/member/entity/Member.java
+++ src/main/java/com/ll/medium/domain/member/member/entity/Member.java
@@ -5,8 +5,13 @@ import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 import static lombok.AccessLevel.PROTECTED;
 
@@ -29,9 +34,16 @@ public class Member {
     private String username;
     private String password;
 
-    public Member(String username, String password) {
-        this.username = username;
-        this.password = password;
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> authorities = new ArrayList<>();
+
+        authorities.add(new SimpleGrantedAuthority("ROLE_MEMBER"));
+
+        if (List.of("system", "admin").contains(username)) {
+            authorities.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
+        }
+
+        return authorities;
     }
 
 }
